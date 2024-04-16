bce7f1c54597f2f9003ef92a60a53ad1a2fa66df
Boris <bvg99@mail.ru>
2024-04-16 20:14:32 +0700

    -

--- MapAndFilter.py
+++ MapAndFilter.py
@@ -3,17 +3,16 @@ numbers = [1, 2, 5, 7, 12, 11, 35, 4, 89, 10]
 # Функция для возведения в квадрат
 def oper(num):
     new_num = num ** 2
-    print(new_num)
+    return new_num
 
 # Функция для возведения в квадрат и отбора нечетных значений
 def odd(num):
     new_num = num ** 2
     if new_num % 2 != 0:
-        print(new_num)
+        return new_num
 
 
 print('Элементы списка возведены в квадрат')
-list(map(oper, numbers))
+print(list(map(oper, numbers)))
 print('Элементы списка возведены в квадрат и выбраны нечетные числа')
-list(filter(odd, numbers))
-
+print(list(map(oper, filter(odd, numbers))))
